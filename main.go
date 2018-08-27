cccba64bc5bf65e466d26d60db66b944a5b3b7ca
messikiller <messikiller@aliyun.com>
2018-08-27 18:19:36 +0800

    reconstruct codes

--- main.go
+++ main.go
@@ -1,49 +1,104 @@
 package main
 
 import (
-    "reporter/util"
-    "github.com/hpcloud/tail"
+    "os"
+    "io/ioutil"
+    "encoding/json"
+    "crypto/tls"
+
+    "github.com/fsnotify/fsnotify"
+    gomail "github.com/go-mail/gomail"
 )
 
-func watch(filename string, to string, subject string) {
-    tails, err := tail.TailFile(filename, tail.Config{
-        ReOpen:    true,
-		Follow:    true,
-		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
-		MustExist: false,
-		Poll:      true,
-    })
+type configMail struct {
+    From string
+    Host string
+    Port int
+    Username string
+    Password string
+}
 
+type configListItem struct {
+    Title string
+    Path string
+    Emails string
+}
+
+type config struct {
+    Mail configMail
+    List []configListItem
+}
+
+func readJson(path string, r *config) {
+    data, err := ioutil.ReadFile(path)
     if err != nil {
         panic(err)
-        return
     }
 
-    var line *tail.Line
-    var body string
+    err = json.Unmarshal(data, &r)
+    if err != nil {
+        panic(err)
+    }
+}
 
-    for true {
+func mail(mail configMail, to string, subject string, body string) {
+    from := mail.From
+    host := mail.Host
+    port := mail.Port
+    username := mail.Username
+    password := mail.Password
+
+    m := gomail.NewMessage()
+
+    m.SetHeader("From", from)
+    m.SetHeader("To", to)
+    m.SetHeader("Subject", subject)
+    m.SetBody("text/html", body)
+
+    d := gomail.NewDialer(host, port, username, password)
+    d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+    if err := d.DialAndSend(m); err != nil {
+        panic(err)
+    }
+}
+
+func watch(filename string, to string, subject string, m configMail) {
+    watcher, _ := fsnotify.NewWatcher();
+    defer watcher.Close();
+
+    watcher.Add(filename);
+
+    f, _ := os.Open(filename)
+    size, _ := f.Seek(0, os.SEEK_END)
+
+    for {
         select {
-        case <-tails.Lines:
-            line = <-tails.Lines
-            body += line.Text
-        default:
-            if body != "" {
-                go util.Mail(to, subject, body)
-                body = ""
+        case ev := <-watcher.Events:
+            if ev.Op & fsnotify.Write == fsnotify.Write {
+                stat, _ := os.Stat(filename)
+                newSize := stat.Size()
+
+                bufferSize := newSize - size
+                buffer := make([]byte, bufferSize)
+                f.ReadAt(buffer, size)
+
+                size = newSize
+
+                body := "<pre>" + string(buffer) + "</pre>"
+
+                go mail(m, to, subject, body)
             }
         }
     }
-
-
 }
 
 func main() {
-    r := []util.ConfigItem{}
-    util.ReadJson("./config.json", &r)
+    r := config{}
+    readJson("./config.json", &r)
 
-    for _, v := range r {
-        go watch(v.Path, v.Emails, "[" + v.Title + "]" + " Exception Found")
+    for _, v := range r.List {
+        go watch(v.Path, v.Emails, "【" + v.Title + "】" + " Exception Found", r.Mail)
     }
 
     ch := make(chan bool)
